274ea8bfc9a775df45597ca461c76a1addc676bc
CHAALAL Mohamed Elmondhir <94637607+elmondhir@users.noreply.github.com>
2024-02-05 20:59:13 +0100

    added login/success page

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path("", views.home, name="home"),
+    path('login/', views.login_view, name='login'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+
+from neo4j import GraphDatabase
+
 # Create your views here.
 
 def home(request):
     return(HttpResponse("Hello World"))
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # database_name = request.POST.get('database_name')
+
+        try:
+            uri = "bolt://localhost:7687"  # Update with your Neo4j database URI
+            graph = GraphDatabase.driver(uri, auth=(username, password))
+            with graph.session() as session:
+                # Attempt to execute a simple query to ensure the connection is valid
+                result = session.run("RETURN 1")
+            
+
+            return render(request, 'success.html', {'username': username})
+
+        except Exception as e:
+            # Connection to Neo4j failed
+            error_message = f"Error connecting to Neo4j: {str(e)}"
+
+            return render(request, 'login.html', {'error': error_message})
+        
+    return render(request, 'login.html')
