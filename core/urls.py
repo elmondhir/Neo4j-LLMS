4f69a58ec3f542cb291542defb8553e286ee8930
CHAALAL Mohamed Elmondhir <94637607+elmondhir@users.noreply.github.com>
2024-02-06 18:41:40 +0100

    Added Url to show db graph

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 from . import views
 urlpatterns = [
-    path("", views.home, name="home"),
+    # path("", views.home, name="home"),
     path('login/', views.login_view, name='login'),
-
+    path('show_graph/<str:database_name>/', views.show_graph, name='show_graph'),
 ]
