08f88c2fb6866c89c8b4ed9a2532d8524ce7d7ce
CHAALAL Mohamed Elmondhir <94637607+elmondhir@users.noreply.github.com>
2024-02-08 18:38:47 +0100

    Added the generate_cypher view

--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+
 
 from neo4j import GraphDatabase
-
+from . import translator
 # Create your views here.
 
 def home(request):
@@ -106,4 +108,18 @@ def show_graph(request, database_name):
     except Exception as e:
         error_message = f"Error querying Neo4j: {str(e)}"
         return render(request, 'error.html', {'error': error_message})
+    
+
+def generate_cypher(request):
+    translation = None
+    database_name = request.POST.get('database_name', '')
+    if request.method == 'POST':
+        input_text = request.POST.get('long_text', '')
+        model_name = request.POST.get('model', '')
+        MY_API_KEY = request.POST.get('hugging_face_token', '')
+        translation = translator.generate(input_text, model_name, MY_API_KEY, False)  # Your summarization code here
+        # Return a partial HTML response
+        return JsonResponse({'translation': translation, 'database_name': database_name})
+    
+    return render(request, "show_graph.html", context={'translation': translation, 'database_name': database_name})
     
